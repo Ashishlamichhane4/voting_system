6f7cfa764ad625147cd19431e5468c00eb8d0234
Ashish Lamichhane <106765312+Ashishlamichhane4@users.noreply.github.com>
2023-05-02 19:39:30 +0545

    sidebar updated

--- Desktop/FInal_project/my-app/src/sidebar.jsx
+++ Desktop/FInal_project/my-app/src/sidebar.jsx
@@ -7,34 +7,51 @@ import GroupAddIcon from '@material-ui/icons/GroupAdd';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import AssessmentIcon from '@material-ui/icons/Assessment';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+// import { Link } from '@material-ui/core';
+import {Link} from "react-router-dom";
 
 export default function Sidebar(selected) {
   return <div className="as">
-    <div className="home">
-      <DashboardRoundedIcon className="home__icon"/><span className='home__comment'>Dashboard</span>
-      {/* yeha dherai kam reapeat vaxa taibhara props use garda better hunxa euta components banai*/}
+    <Link to="/">
+      <div className="home">
+         <DashboardRoundedIcon className="home__icon"/><span className='home__comment'>Dashboard</span>
+          {/* yeha dherai kam reapeat vaxa taibhara props use garda better hunxa euta components banai*/}
 
-    </div>
-    <div className="home">
+      </div>
+    </Link>
+    <Link to="/election">
+         <div className="home">
 
-      <HowToVoteRoundedIcon className='home__icon'/><span className='home__comment'>Election</span>
-    </div>
+             <HowToVoteRoundedIcon className='home__icon'/><span className='home__comment'>Election</span>
+         </div>
+    </Link>
+    <Link to="/candidates">
+      <div className="home">
+        <SupervisorAccountIcon className='home__icon'/><span className='home__comment'>Candidates</span>
 
-    <div className="home">
-      <SupervisorAccountIcon className='home__icon'/><span className='home__comment'>Candidates</span>
-
-    </div>
+      </div>
+    </Link>
 <hr className="line"/>
+    <Link to="/voters">
+      <div className="home">
+        <GroupAddIcon className='home__icon'/><span className='home__comment'>Voters</span>
+     </div>
+
+    </Link>
+    <Link to="/constituency">
+      <div className="home">
+        <CheckCircleIcon className='home__icon'/><span className='home__comment'>Constituency</span>
+      </div>
+
+    </Link >
+    <Link to="results">
+      <div className="home">
+        <AssessmentIcon className='home__icon'/><span className='home__comment'>Results</span>
+      </div>
+
+    </Link>
+
     
-    <div className="home">
-      <GroupAddIcon className='home__icon'/><span className='home__comment'>Voters</span>
-    </div>
-    <div className="home">
-      <CheckCircleIcon className='home__icon'/><span className='home__comment'>Constituency</span>
-    </div>
-    <div className="home">
-      <AssessmentIcon className='home__icon'/><span className='home__comment'>Results</span>
-    </div>
     
     
   </div>
