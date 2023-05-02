c154d1f553a9f3fabde1793b73d937feecaf1064
Ashish Lamichhane <106765312+Ashishlamichhane4@users.noreply.github.com>
2023-05-02 19:20:21 +0545

    work with react router dom but not succed

--- Desktop/FInal_project/my-app/src/login.jsx
+++ Desktop/FInal_project/my-app/src/login.jsx
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Switch,
+    Link
+  } from "react-router-dom";
 import LoginForm from './LoginForm.js';
 import Information from './Info.js';
 import Sidebar from './sidebar.jsx';
@@ -9,17 +16,13 @@ import ElectionDetails from "./Electiondetails.jsx";
 import Adminpage from './Admin_Page.js';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
-
-
-
 import "./login.css";
-
 function Login() {
     const adminUser = {
         user: "admin",
         email: "ashishlamichhane437@gmail.com",
         password: "pass1"
-    }
+    };
     const [user, setUser] = useState({ user: "", email: "" });
     const [error, setError] = useState("");
     const Login = details => {
@@ -29,55 +32,112 @@ function Login() {
             setUser({
                 user: details.user,
                 email: details.email
-
-            })
-
-        }
-        else {
+            });
+        } else {
             console.log("sorry details not match");
-            setError("sorry details not match")
+            setError("sorry details not match");
         }
-
-    }
+    };
     const Logout = () => {
         console.log("Logout");
-        setUser({ name: "", email: "" });
-
-    }
-
-
+        setUser({ user: "", email: "" });
+    };
 
     return (
         <div className="Login">
-            {(user.email != "") ? (
-                <div className="welcome">
-                    <Adminpage/>
-                    <div className="app__page">
-
-                       <Sidebar/>
-                       <Information/>
-                       {/* <Upload/> */}
-                       {/* <ElectionDetails/> */}
-                    </div>
-                    <div className="home1" onClick={Logout}>
-
-                      <ExitToAppIcon className='home__icon'  /><span className='home__comment'>Logout</span>
-                      
-                    </div>
-
+            {user.email != "" ? (
+                 <div className="welcome">
                     
-                    {/* <h2>WELCOme,<span>{user.name}</span></h2> */}
-                    {/* <button onClick={Logout}>Logout</button> */}
-                </div>
+      <Router> 
+        {/* <Header/>  */}
+        <Adminpage/>
+    
+        <Switch>
+          <Route path="/">
+          
+             <div className='app__page'>
+                <Sidebar/>
+                {/* <SearchPage/> */}
+                <Information/>
+                
+              </div>  
+          </Route>
+          <Route exact path="/mm">
+               <div className='app__page'>
+                 <Sidebar/>
+                 
 
+                  {/* <Recommended/>  */}
+                  <ElectionDetails/>
+             </div> 
+         </Route> 
+          <Route exact path="/Candidates">
+               <div className='app__page'>
+                 <Sidebar/>
+                 
+
+                  {/* <Recommended/>  */}
+                  <Upload/>
+             </div>  
+          </Route>
+          <Route exact path="/Candidates">
+               <div className='app__page'>
+                 <Sidebar/>
+                 
+
+                  {/* <Recommended/>  */}
+                  <Upload/>
+             </div>  
+          </Route>
+          
+          <Route exact path="/Candidates">
+               <div className='app__page'>
+                 <Sidebar/>
+                 
+
+                  {/* <Recommended/>  */}
+                  <Upload/>
+             </div>  
+          </Route>
+
+
+        </Switch>
+    </Router>
+
+            
+
+
+
+                 {/* <Adminpage/> */}
+                 {/* <div className="app__page"> */}
+
+                    {/* <Sidebar/> */}
+                    {/* <Information/> */}
+                    {/* <Upload/> */}
+                    {/* <ElectionDetails/> */}
+                 {/* </div> */}
+                 <div className="home1" onClick={Logout}>
+
+                   <ExitToAppIcon className='home__icon'  /><span className='home__comment'>Logout</span>
+                   
+                 </div>
+
+                 
+                 {/* <h2>WELCOme,<span>{user.name}</span></h2> */}
+                 {/* <button onClick={Logout}>Logout</button> */}
+             </div>
+              
             ) : (
                 <LoginForm Login={Login} error={error} />
-            )
-            }
-
-
+            )}
+            {user.email != "" && (
+                <div className="home1" onClick={Logout}>
+                    <ExitToAppIcon className='home__icon' /><span className='home__comment'>Logout</span>
+                </div>
+            )}
         </div>
     );
 }
 
 export default Login;
+
